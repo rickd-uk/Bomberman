8db9a33ff61a2a57af00fc0352061bddf691ea5f
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-02-19 14:12:14 +0900

    add - tilemap; player; movement controller

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,6 +1,62 @@
 using UnityEngine;
 
-public class Scripts : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
-   
+   public Rigidbody2D Rigidbody { get; private set; }
+
+    private Vector2 direction = Vector2.down;
+    public float speed = 5.0f;
+
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
+
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(inputUp))
+        {
+            SetDirection(Vector2.up);
+        }
+        else if (Input.GetKey(inputDown))
+        {
+            SetDirection(Vector2.down);
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            SetDirection(Vector2.left);
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            SetDirection(Vector2.right);
+        }
+        else
+        {
+            SetDirection(Vector2.zero);
+        }
+    }
+
+    private void Update()
+    {
+        HandleInput();
+     }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = Rigidbody.position;
+        Vector2 translation = speed * Time.fixedDeltaTime * direction;
+
+        Rigidbody.MovePosition(position + translation);
+    }
+
+    private void SetDirection(Vector2 newDir)
+    {
+        direction = newDir;
+    }
 }
